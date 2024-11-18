a5eafc1c3ddd9c9102eba885d8e1214eac0ff8f9
IrisCode <150204523+frank777ch@users.noreply.github.com>
2024-11-18 16:45:05 -0500

    presentacion

---
+++ batch/menu.bat
@@ -0,0 +1,33 @@
+:: Ocultar todos los comandos
+:: @ -> para ocultar el primer echo off
+@echo off
+
+:: Cambiar a UTF-8
+chcp 65001 >nul
+
+echo.
+echo ╔══════════════════════════════════════════════════════════════════════════════╗
+echo ║ ██████╗ ██╗███████╗███╗   ██╗██╗   ██╗███████╗███╗   ██╗██╗██████╗  ██████╗  ║
+echo ║ ██╔══██╗██║██╔════╝████╗  ██║██║   ██║██╔════╝████╗  ██║██║██╔══██╗██╔═══██╗ ║
+echo ║ ██████╔╝██║█████╗  ██╔██╗ ██║██║   ██║█████╗  ██╔██╗ ██║██║██║  ██║██║   ██║ ║
+echo ║ ██╔══██╗██║██╔══╝  ██║╚██╗██║╚██╗ ██╔╝██╔══╝  ██║╚██╗██║██║██║  ██║██║   ██║ ║
+echo ║ ██████╔╝██║███████╗██║ ╚████║ ╚████╔╝ ███████╗██║ ╚████║██║██████╔╝╚██████╔╝ ║
+echo ║ ╚═════╝ ╚═╝╚══════╝╚═╝  ╚═══╝  ╚═══╝  ╚══════╝╚═╝  ╚═══╝╚═╝╚═════╝  ╚═════╝  ║
+echo ╠══════════════════════════════════════════════════════════════════════════════╣
+echo ║ Este programa fue desarrollado como parte de un trabajo grupal para el curso ║
+echo ║ de Sistemas Operativos a cargo del profesor Jaime Pariona que consiste en la ║
+echo ║ creación de un menú de opciones para manejar archivos                        ║
+echo ╠═══════════╦══════════════════════════════════════════════════════════════════╣
+echo ║   Grupo   ║ Integrantes                                                      ║
+echo ╠═══════════╬══════════════════════════════════════════════════════════════════╣
+echo ║           ║ Frank Kevin Condor Huarhuachi                                    ║
+echo ║     5     ║ Vilberto Alberto Patricio Julca                                  ║
+echo ║           ║ Pedro Josue Sota Rios                                            ║
+echo ║           ║ Luiz Ignacio Quineche Casana                                     ║
+echo ╠═══════════╬══════════════════════════════════════════════════════════════════╣
+
+:: Poner en pausa la ejecución de comandos del archivo hasta pulsar cualquier tecla
+pause
+
+:: Salir del archivo
+exit
