48f5fc85945dd2529c77538d834d6ea1b1ee41f7
Vilberto02 <albertojulcapatricio@gmail.com>
2024-11-18 23:39:41 -0500

    Add option two to the menu

--- batch/menu.bat
+++ batch/menu.bat
@@ -127,11 +127,11 @@ echo                ╚═══════════════════
 echo.
 echo                 %cd%
 echo.
-echo                ╔═══════════════════════════════════════════════════════════════════════════════════╗
-echo                ║                             CONTENIDO DEL DIRECTORIO                              ║
-echo                ╠═════════════════════╦═══════════════════╦═════════════════════════════════════════╣
-echo                ║      FECHA          ║       TAMAÑO      ║              NOMBRE                     ║
-echo                ╠═════════════════════╬═══════════════════╬═════════════════════════════════════════╣
+echo                ╔═══════════════════════════════════════════════════════════════════════════════════════════╗
+echo                ║                             CONTENIDO DEL DIRECTORIO                                      ║
+echo                ╠═════════════════════╦═══════════════════╦═════════════════════════════════════════════════╣
+echo                ║      FECHA          ║       TAMAÑO      ║              NOMBRE                             ║
+echo                ╠═════════════════════╬═══════════════════╬═════════════════════════════════════════════════╣
 
 :: Mostrar detalles de archivos y carpetas con formato fijo
 setlocal enabledelayedexpansion
@@ -143,17 +143,17 @@ for /f "tokens=1,2,3*" %%A in ('dir /a:-d /o:d ^| findstr /r "^[0-9]"') do (
     :: Formatear columnas con longitud fija manualmente
     set "paddedFecha=!fecha!                    "
     set "paddedTamano=!tamano!                  "
-    set "paddedNombre=!nombre!                                       "
+    set "paddedNombre=!nombre!                                          "
 
     set "paddedFecha=!paddedFecha:~0,20!"
     set "paddedTamano=!paddedTamano:~0,18!"
-    set "paddedNombre=!paddedNombre:~0,40!"
+    set "paddedNombre=!paddedNombre:~0,48!"
 
     echo                ║ !paddedFecha!║ !paddedTamano!║ !paddedNombre!║
 )
 endlocal
 
-echo                ╚═════════════════════╩═══════════════════╩═════════════════════════════════════════╝
+echo                ╚═════════════════════╩═══════════════════╩═════════════════════════════════════════════════╝
 
 :: Poner en pausa la ejecución de comandos del archivo hasta pulsar cualquier tecla
 :: ">nul" -> ocultar texto que sale por defecto
@@ -167,8 +167,24 @@ cls
 goto MainMenu
 
 :Opt2Menu
+
+:: Mostrar encabezado
+echo                ╔══════════════════════════════╗
+echo                ║ Cambiar el Directorio actual ║
+echo                ╚══════════════════════════════╝
 echo.
-echo Escogiste la opción 2
+echo                 %cd%
+echo.
+
+
+
+set /p "ruta=‎               INGRESE LA RUTA AL CUAL DESEA DIRIGIRSE: "
+:: Limpiar pantalla
+cls
+
+cd /d "%ruta%"
+
+goto Opt1Menu
 
 :: Poner en pausa la ejecución de comandos del archivo hasta pulsar cualquier tecla
 :: ">nul" -> ocultar texto que sale por defecto
